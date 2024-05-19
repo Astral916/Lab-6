6847b96f5eb6cd920a70d40fd4c934048866e716
Astral916 <astral916@yandex.ru>
2024-05-19 23:56:02 +0300

    Add simple console calculator

--- MyProject/Program.cs
+++ MyProject/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace MyProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите операцию (+, -, *, /):");
+            string operation = Console.ReadLine();
+
+            Console.WriteLine("Введите второе число:");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            double result = 0;
+
+            switch (operation)
+            {
+                case "+":
+                    result = num1 + num2;
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    break;
+                case "/":
+                    result = num1 / num2;
+                    break;
+                default:
+                    Console.WriteLine("Неверная операция");
+                    return;
+            }
+
+            Console.WriteLine($"Результат: {result}");
+        }
+    }
+}
+
