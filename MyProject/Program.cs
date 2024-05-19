205be910410759d71f7b995a735ff9330d73cbd3
Astral916 <astral916@yandex.ru>
2024-05-20 00:28:14 +0300

    Add square operation

--- MyProject/Program.cs
+++ MyProject/Program.cs
@@ -9,27 +9,43 @@ namespace MyProject
             Console.WriteLine("Введите первое число:");
             double num1 = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("Введите операцию (+, -, *, /):");
+            Console.WriteLine("Введите операцию (+, -, *, /, ^2):");
             string operation = Console.ReadLine();
-
-            Console.WriteLine("Введите второе число:");
-            double num2 = Convert.ToDouble(Console.ReadLine());
 
             double result = 0;
 
             switch (operation)
             {
                 case "+":
+                    Console.WriteLine("Введите второе число:");
+                    double num2 = Convert.ToDouble(Console.ReadLine());
                     result = num1 + num2;
                     break;
                 case "-":
-                    result = num1 - num2;
+                    Console.WriteLine("Введите второе число:");
+                    double num3 = Convert.ToDouble(Console.ReadLine());
+                    result = num1 - num3;
                     break;
                 case "*":
-                    result = num1 * num2;
+                    Console.WriteLine("Введите второе число:");
+                    double num4 = Convert.ToDouble(Console.ReadLine());
+                    result = num1 * num4;
                     break;
                 case "/":
-                    result = num1 / num2;
+                    Console.WriteLine("Введите второе число:");
+                    double num5 = Convert.ToDouble(Console.ReadLine());
+                    if (num5 != 0)
+                    {
+                        result = num1 / num5;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ошибка: деление на ноль!");
+                        return;
+                    }
+                    break;
+                case "^2":
+                    result = num1 * num1;
                     break;
                 default:
                     Console.WriteLine("Неверная операция");
@@ -40,4 +56,3 @@ namespace MyProject
         }
     }
 }
-
